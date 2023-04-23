e76598dd900aafd675f60f80f8fed892210cbd92
codma <ruddud1123@naver.com>
2023-04-23 17:29:38 +0900

    Rename: 폴더 구조 초기화

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,5 +1,22 @@
 <template>
   <div>
-    index..
+    <!-- 진원 -->
+    <HeaderLayout />         
+    <LandingLayout />
+
+    <!-- 준하 -->
+    <SearchLayout />
+    <HowToLayout />
+
+    <!-- 진원 -->
+    <FooterLayout />
   </div>
 </template>
+
+<script setup lang="ts">
+import HeaderLayout from '~/layouts/HeaderLayout.vue';
+import LandingLayout from '~/layouts/index/LandingLayout.vue'
+import SearchLayout from '~/layouts/index/HowToLayout.vue'
+import HowToLayout from '~/layouts/index/HowToLayout.vue'
+import FooterLayout from '~/layouts/FooterLayout.vue'
+</script>
