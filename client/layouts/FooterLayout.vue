8fbd10468f0ed1448fcf1ed484c216713eae0c34
진원 <89727516+z1nun@users.noreply.github.com>
2023-04-24 17:51:36 +0900

    design : make Footer Layout

--- client/layouts/FooterLayout.vue
+++ client/layouts/FooterLayout.vue
@@ -1,13 +1,85 @@
 <template>
-  <div>
-
-  </div>
+  <footer>
+    <div class="Container">
+      <div class="LogoImg">LOGO</div>
+      <div class="InfoWrap">
+        <div class="Info">        
+          <span>TEAM NAME : 착한소비</span>
+          <span>info</span>
+          <span>cs</span>
+          <span>개인정보</span>
+        </div>
+        <span>Title email : text </span>
+        <span>other info </span>
+      </div>
+    </div>
+  </footer>
 </template>
 
 <script setup lang="ts">
 
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+
+footer {
+  background-color : #F5F7FF;
+}
+.Container {
+  display: flex;
+  padding: 48px 20px;
+  margin : 0 auto;
+  width: 320px;
+  height: 188px;
+  gap: 20px;
+
+
+  @media (min-width: 768px) {
+    padding: 80px 20px;
+    width: 728px;
+    height: 105px;
+  }
+  @media (min-width: 1024px) {
+    width: 984px;
+  }
+  @media (min-width: 1440px) {
+    width: 1120px;
+  }
+  @media (min-width: 1640px) {
+    width: 1496px;
+  }
+}
+.LogoImg {
+  width: 67px;
+  height: 36px;
+  font-size: 24px;
+  font-weight: 700;
+}
+
+.InfoWrap {
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+  
+  span {
+    color: #8C95A1;
+  }
+}
+.Info {
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+
+  @media (min-width: 768px) {
+    flex-direction: row;
+    gap: 40px;
+  }
+  span {  
+    color : #343E4C;
+    font-weight: 600;
+  }
+
+
+}
 
 </style>
