f9762687d9bbd432ba9a4c668bdebbe6eeefb4de
codma <ruddud1123@naver.com>
2023-04-25 13:30:05 +0900

    Fix: merge 오류 수정

--- client/nuxt.config.ts
+++ client/nuxt.config.ts
@@ -1,9 +1,4 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-<<<<<<< HEAD
-  css: ['@/assets/fonts/main.css'],  
-})
-=======
   css: ['@/assets/fonts/main.css']
 })
->>>>>>> 9ddbcb2b46fa2c5483af285d117df024d5e561da
