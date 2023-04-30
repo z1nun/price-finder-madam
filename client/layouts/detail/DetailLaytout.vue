34adc232988886dc9c386dbcf27d9b4df60dc0ca
codma <ruddud1123@naver.com>
2023-04-30 13:10:00 +0900

    feat: detailLayout, MapLayout init

---
+++ client/layouts/detail/DetailLaytout.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    detail
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
--- client/pages/detail.vue
+++ client/pages/detail.vue
@@ -1,16 +1,13 @@
 <template>
   <section>
-    <div>
-      1
-    </div>
-    <div>
-      2
-    </div>
+    <DetailLaytout />
+    <MapLayout />
   </section>
 </template>
 
 <script setup lang="ts">
-
+import DetailLaytout from '~/layouts/detail/DetailLaytout.vue';
+import MapLayout from '~/layouts/detail/MapLayout.vue';
 </script>
 
 <style scoped lang="scss">
