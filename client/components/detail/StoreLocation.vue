053d69b67a576034628b4251e8d3e86553c252cd
진원 <89727516+z1nun@users.noreply.github.com>
2023-05-08 00:22:16 +0900

    design : StoreCard 컴포넌트 분리

---
+++ client/components/detail/StoreLocation.vue
@@ -0,0 +1,16 @@
+<template>
+  <article>
+    <h3>위치정보</h3>
+    <ul>
+      <li>{{ location }}</li>
+    </ul>
+  </article>
+</template>
+
+<script setup lang="ts">
+const location: string = '서울특별시 광진구 자양번영로 13'
+</script>
+
+<style lang="scss" scoped>
+@import '~/assets/style/StoreCardStyle.scss';
+</style>
