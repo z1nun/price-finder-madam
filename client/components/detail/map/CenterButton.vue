798921afd996c31616c23246a5405a65322d9da0
codma <ruddud1123@naver.com>
2023-05-24 20:33:47 +0900

    feat: 지도에 중앙 마커로 이동하는 버튼 추가

---
+++ client/components/detail/map/CenterButton.vue
@@ -0,0 +1,42 @@
+<template>
+  <article>
+    <img src="~/assets/img/detail/target.svg" @click="emit('focusCenter')"/>
+  </article>
+</template>
+
+<script setup lang="ts">
+
+const emit = defineEmits<{ (e: 'focusCenter'): void }>()
+
+</script>
+
+<style scoped lang="scss">
+article {
+  position: absolute;
+  right: 0;
+  top: 20%;
+  transform: translateY(-20%);
+  z-index: 0;
+  margin-right: 2.5rem;
+  box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.3);
+  background-color: white;
+  border-radius: 8px;
+  width: 48px;
+  height: 48px;  
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-around;
+
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+
+  img {
+    width: 24px;
+    height: 24px;
+    cursor: pointer;    
+  }
+}
+</style>
