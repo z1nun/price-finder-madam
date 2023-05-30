ad53fd5d704851706aaf87a798126b1abfd45403
진원 <89727516+z1nun@users.noreply.github.com>
2023-05-30 16:31:48 +0900

    feat : 홈화면 카드 라우팅 수정

--- client/components/index/search/SearchArticles.vue
+++ client/components/index/search/SearchArticles.vue
@@ -1,13 +1,8 @@
 <template>
   <article class="SearchArticles">
     <!-- <RecommendCards /> -->
-    <template v-if="!indexCards.loading">      
-      <RecommendCard
-        v-for="(recommendCard, i) in recommendCards"
-        :key="i"
-        :recommend-card="recommendCard"
-        @click="router.push('/detail')"
-      />  
+    <template v-if="!indexCards.loading">
+      <RecommendCard v-for="(recommendCard, i) in recommendCards" :key="i" :recommend-card="recommendCard" />
       <RecommendMore @click="router.push('/search')" />
     </template>
   </article>
@@ -22,7 +17,9 @@ import { StoreCard } from '~/types/baseTypes'
 
 const router = useRouter()
 const { divice } = useDisplay()
-const { asyncStates: { indexCards }} = useStore()
+const {
+  asyncStates: { indexCards },
+} = useStore()
 
 const displayCount = computed<number>(() => {
   switch (divice.value) {
@@ -40,19 +37,17 @@ const displayCount = computed<number>(() => {
 })
 
 const recommendCards = computed<StoreCard[]>(() => indexCards.data?.slice(0, displayCount.value))
-
 </script>
 
 <style scoped lang="scss">
-.SearchArticles {    
+.SearchArticles {
   margin-top: 40px;
-  max-width: 1456px;  
+  max-width: 1456px;
 
-  gap: 40px 48px; 
-  
+  gap: 40px 48px;
+
   display: flex;
   flex-wrap: wrap;
   justify-content: center;
-  
 }
 </style>
