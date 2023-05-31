01f1e357cecedaf6ba5a540f89721c6963e089fc
진원 <89727516+z1nun@users.noreply.github.com>
2023-05-31 13:48:42 +0900

    fix : 하단 드롭시트 버그 수정

--- client/layouts/search/SearchLayout.vue
+++ client/layouts/search/SearchLayout.vue
@@ -15,11 +15,14 @@ import SearchItem from '~/components/search/SearchItem.vue'
 <style lang="scss" scoped>
 section {
   width: 80%;
+  height: 100vh;
   background-color: #f9fafb;
   display: flex;
   flex-direction: column;
   gap: 8px;
+  z-index: 3;
   @media (min-width: 768px) {
+    position: static;
     width: 360px;
   }
 }
