3552a14f7b4f7872eb9fe2653331e4a3d6894e78
진원 <89727516+z1nun@users.noreply.github.com>
2023-06-05 11:45:09 +0900

    refector : detail 페이지 내 하단 footer 레이아웃 수정, 로고 이미지 클릭시 홈화면 라우트

--- client/layouts/FooterLayout.vue
+++ client/layouts/FooterLayout.vue
@@ -1,7 +1,7 @@
 <template>
   <footer>
     <div class="Container">
-      <img src="~/assets/img/footerLogo.svg" />
+      <img src="~/assets/img/footerLogo.svg" @click="push(`/`)" :style="{ cursor: 'pointer' }" />
       <div class="InfoWrap">
         <div class="Info">
           <span>TEAM NAME : 아지매들</span>
@@ -13,7 +13,10 @@
   </footer>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { Router } from 'vue-router'
+const { push } = useRouter()
+</script>
 
 <style scoped lang="scss">
 footer {
@@ -23,18 +26,28 @@ footer {
     max-width: 360px;
     .Container {
       display: flex;
+      justify-content: center;
+      align-items: center;
       margin: 0 auto;
-      padding-top: 20px;
+      padding: 24px 20px;
       width: 320px;
+      height: 190px;
       gap: 20px;
+    }
+    .InfoWrap {
+      display: flex;
+      gap: 16px;
     }
     .Info {
       display: flex;
       flex-direction: column;
+      gap: 20px;
     }
     img {
+      width: 124px;
+      height: 60px;
       position: relative;
-      bottom: 70px;
+      bottom: 20px;
     }
   }
 
